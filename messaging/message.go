98aceb9b3d38a302b26c240035f663c3a059a14e
A. Ruiz <40408539+maestre3d@users.noreply.github.com>
2023-01-14 20:48:16 -0600

    Remove unused messaging components

--- messaging/message.go
+++
@@ -1,6 +0,0 @@
-package messaging
-
-type Message struct {
-	ID   string `json:"message_id"`
-	Data []byte `json:"data"`
-}
--- messaging/reader.go
+++
@@ -1,7 +0,0 @@
-package messaging
-
-import (
-	"context"
-)
-
-type ReaderFunc func(ctx context.Context)
